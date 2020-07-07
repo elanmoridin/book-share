ec6e2042a0204c11d222428e4b3215637cff2618
Zach Doll <zach.doll@gmail.com>
2020-07-07 13:39:14 -0500

    created controllers page and setup a books model schema to be used

---
+++ models/books.js
@@ -0,0 +1,15 @@
+// required linking of dependencies
+const mongoose = require('mongoose')
+
+// creating the schema for the book information to be stored in the DB
+const bookSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    author: {type: String},
+    genre: {type: String}
+})
+
+// creating a variable to allow other pages to pull info from this schema
+const Books = mongoose.model('Books', bookSchema)
+
+// exporting this schema for other pages to use
+module.exports = Books
