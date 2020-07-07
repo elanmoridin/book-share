644a164ad218f3ec91885dd11e144bbdbb708ca6
Zach Doll <zach.doll@gmail.com>
2020-07-07 16:26:02 -0500

    added seed for database testing and made sure redirect to root index from it works

--- controllers/bookshare.js
+++ controllers/bookshare.js
@@ -11,5 +11,30 @@ router.get('/', (req, res) => {
     res.send('Hello I am Root')
 })
 
+// SEED ROUTE for DB //
+router.get('/seed', (req, res) => {
+    Books.deleteMany(() => {
+        Books.create([
+            {
+                title: "The Eye of the World",
+                author: "Robert Jordan",
+                genre: "Fantasy",
+            },
+            {
+                title: "The Complete Robot",
+                author: "Isaac Asimov",
+                genre: "Science Fiction",
+            },
+            {
+                title: "The 4-Hour Chef",
+                author: "Tim Ferriss",
+                genre: "Self-Help",
+            }
+        ], (err, data) => {
+            res.redirect('/')
+        })
+    })
+})
+
 // export router for use in index
 module.exports = router
